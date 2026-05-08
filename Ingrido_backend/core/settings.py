58e4b7ed530ae73567f54083122475d217700a4d
Asma <asmaimran404@gmail.com>
2026-05-08 20:43:36 +0500

    merge resolved

--- Ingrido_backend/core/settings.py
+++ Ingrido_backend/core/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ingrido_db',
         'USER': 'postgres',
-        'PASSWORD': 'anb',
+        'PASSWORD': 'pakpost000',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
