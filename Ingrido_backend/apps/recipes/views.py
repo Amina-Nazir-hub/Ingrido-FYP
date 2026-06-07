f2d418f46a8534fb36d4c999cb4b0960f43fc6ac
AsmaImran-1 <asmaimran404@gmail.com>
2026-06-08 02:50:08 +0500

    Refactor to use get_groq_client for GROQ client
    Refactor to use get_groq_client for GROQ client

--- Ingrido_backend/apps/recipes/views.py
+++ Ingrido_backend/apps/recipes/views.py
@@ -20,7 +20,7 @@ from apps.common.services import get_ai_generated_image
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-groq_client = Groq(api_key=GROQ_API_KEY) if GROQ_API_KEY else None
+groq_client = get_groq_client()
 
 # ========== CITY & RECIPE VIEWS ==========
 
